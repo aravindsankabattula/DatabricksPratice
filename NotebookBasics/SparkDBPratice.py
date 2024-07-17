6ecc0ac97ec457ecbbe6237b713c6963cdfa94ba
aravindsankabattula <aravindsankabattula>
2024-07-17 10:14:27 +0000

    Commit

--- NotebookBasics/SparkDBPratice.py
+++ NotebookBasics/SparkDBPratice.py
@@ -1,2 +1,19 @@
 # Databricks notebook source
 print("starting now")
+
+# COMMAND ----------
+
+dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+
+# COMMAND ----------
+
+# MAGIC %run ./example1-setup
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC select * from demo_tmp_vw
+
+# COMMAND ----------
+
+spark.table("demo_tmp_vw").show()
